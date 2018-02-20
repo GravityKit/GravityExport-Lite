06df87c5658a6ac359e2097c18799d4121df8092
Doeke Norg <doekenorg@gmail.com>
2018-02-20 21:58:03 +0100

    Removing some SQUID Media remnands. I no longer work there, but this was always my project; so I wanted to keep developing it :)

--- gfexcel.php
+++ gfexcel.php
@@ -2,10 +2,10 @@
 /**
  * Plugin Name:     Gravity Forms Entries in Excel
  * Description:     Download all entries for a form using a unique and hashed URL
- * Author:          SQUID Media
- * Author URI:      https://www.squidmedia.nl
+ * Author:          Doeke Norg
+ * Author URI:      https://paypal.me/doekenorg
  * Text Domain:     gfexcel
- * Version:         1.1.0
+ * Version:         1.2.0
  *
  * @package         GFExcel
  */
