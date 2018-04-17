206f4642d1c37e42bddb345f1aadb72299110b0b
Tom Macdonald <tmacdonald@yelsterdigital.com>
2018-04-17 10:56:15 +0200

    version update

--- gfexcel.php
+++ gfexcel.php
@@ -5,7 +5,7 @@
  * Author:          Doeke Norg
  * Author URI:      https://paypal.me/doekenorg
  * Text Domain:     gf-entries-in-excel
- * Version:         1.3.0
+ * Version:         1.2.4
  *
  * @package         GFExcel
  */
