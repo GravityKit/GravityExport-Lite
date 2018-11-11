e9a44aff301e1a52f527226927013f820a29482b
Doeke Norg <doekenorg@gmail.com>
2018-11-11 20:09:19 +0100

    throw deprecated error for addressfield

--- src/Field/AddressField.php
+++ src/Field/AddressField.php
@@ -2,9 +2,16 @@
 
 namespace GFExcel\Field;
 
+use GF_Field;
+
 /**
  * @deprecated Use SeperableField instead.
  */
 class AddressField extends SeparableField
 {
+    public function __construct(GF_Field $field)
+    {
+        trigger_error(__CLASS__ . ' is deprecated. Please use ' . SeparableField::class, E_USER_DEPRECATED);
+        parent::__construct($field);
+    }
 }
