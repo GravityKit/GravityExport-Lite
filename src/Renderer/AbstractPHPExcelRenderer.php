de250517d9c135c7b2aa5b2b0b758fbc9debfe4b
SalvaG <sgregori@users.noreply.github.com>
2019-01-30 16:55:48 +0100

    Added ob_end_clean(), for cleaning buffer for preventing file corruption

--- src/Renderer/AbstractPHPExcelRenderer.php
+++ src/Renderer/AbstractPHPExcelRenderer.php
@@ -63,7 +63,7 @@ abstract class AbstractPHPExcelRenderer
             header('Last-Modified: ' . gmdate('D, d M Y H:i:s') . ' GMT'); // always modified
             header('Cache-Control: cache, must-revalidate'); // HTTP/1.1
             header('Pragma: public'); // HTTP/1.0
-
+            ob_end_clean(); // Cleaning buffer for preventing file corruption
             $objWriter->save('php://output');
         } catch (\Exception $e) {
             //in case of php5.x
