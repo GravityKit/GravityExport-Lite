ddde132562ec9120b34391600e32847662d3172a
Doeke Norg <doekenorg@gmail.com>
2020-05-13 22:04:26 +0200

    Added first ever unit test for the plugin. How far we have come.

--- tests/Values/CurrencyValueTest.php
+++ tests/Values/CurrencyValueTest.php
@@ -6,6 +6,10 @@ use GFExcel\Tests\TestCase;
 use GFExcel\Values\CurrencyValue;
 use PHPUnit\Framework\MockObject\MockObject;
 
+/**
+ * Unit tests for {@see CurrencyValue}.
+ * @since $ver$
+ */
 class CurrencyValueTest extends TestCase
 {
     /**
@@ -28,12 +32,38 @@ class CurrencyValueTest extends TestCase
      */
     public function setup(): void
     {
-        $this->gf_field = $this->createMock(\GF_Field::class);
+        $this->gf_field = $this->getMockBuilder('GF_Field')->getMock();
         $this->value_object = new CurrencyValue(1000, $this->gf_field);
     }
 
+    /**
+     * Test case for {@see CurrencyValue::getFormat()} and {@see CurrencyValue::setFormat()}
+     * @since $ver$
+     */
     public function testFormat(): void
     {
-        $this->assertSame('$', $this->value_object->getFormat());
+        $this->assertSame(CurrencyValue::FORMAT_CURRENCY_NONE, $this->value_object->getFormat());
+        $this->value_object->setFormat('test');
+        $this->assertSame('test', $this->value_object->getFormat());
+    }
+
+    /**
+     * Test case for {@see CurrencyValue::getSymbol()} and {@see CurrencyValue::setSymbol()}
+     * @since $ver$
+     */
+    public function testSymbol(): void
+    {
+        $this->assertSame('$', $this->value_object->getSymbol());
+        $this->value_object->setSymbol('test');
+        $this->assertSame('test', $this->value_object->getSymbol());
+    }
+
+    /**
+     * Test case for {@see CurrencyValue::isNumeric()}.
+     * @since $ver$
+     */
+    public function testIsNumeric(): void
+    {
+        $this->assertTrue($this->value_object->isNumeric());
     }
 }
