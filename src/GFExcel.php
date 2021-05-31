42bf2635abaee7e7aec3699b31a07d2e85ab7a1c
Vlad <vlad@gravityview.co>
2021-05-31 12:59:52 -0400

    Bump version to 1.8.13

--- src/GFExcel.php
+++ src/GFExcel.php
@@ -35,7 +35,7 @@ class GFExcel
      * @since 1.0.0
      * @var string
      */
-    public static $version = '1.8.12';
+    public static $version = '1.8.13';
 
     /**
      * The endpoint slug of the plugin.
