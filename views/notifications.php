8305ee69ff6035d4651e6c996912801a4229c4a5
Zack Katz <zack@katz.co>
2021-09-06 21:37:42 -0400

    Fix translation of errors

--- views/notifications.php
+++ views/notifications.php
@@ -27,8 +27,8 @@ foreach ($notifications as $notification):
     <?php } ?>
             class="notice<?php echo $dismissible; ?> notice-<?php echo $notification->getType(); ?>">
         <p>
-            <strong><?php _e('Gravity Forms Entries in Excel', GFExcel::$slug); ?></strong><br/>
-            <?php _e($notification->getMessage(), GFExcel::$slug); ?>
+            <strong><?php esc_html_e('Gravity Forms Entries in Excel', GFExcel::$slug); ?></strong><br/>
+            <?php echo esc_html( $notification->getMessage() ); ?>
         </p>
     </div>
 <?php endforeach;
