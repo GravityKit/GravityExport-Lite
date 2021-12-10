b14f231ebf66a8bc112191abc5862f4be30fa3b9
Zack Katz <zack@katz.co>
2021-12-10 11:00:12 -0500

    Move logic out of the function call & add inline docs

--- src/Field/FileUploadField.php
+++ src/Field/FileUploadField.php
@@ -44,9 +44,14 @@ class FileUploadField extends BaseField
 	 * @return bool Whether the uploads should be shown as a column.
 	 */
 	private function showFileUploadsAsColumn() {
+
+		// The default value of `true` will not be returned by get_plugin_setting(); it will return null by default.
+		// So we apply the ?? operator and return true as the default value.
+		$fileuploads_enabled = GFExcelAdmin::get_instance()->get_plugin_setting( 'fileuploads_enabled' ) ?? true;
+
 		return gf_apply_filters( [
 			'gfexcel_field_fileuploads_enabled',
 			$this->field->formId,
-		], (bool) ( GFExcelAdmin::get_instance()->get_plugin_setting( 'fileuploads_enabled' ) ?? true ) );
+		], (bool) $fileuploads_enabled );
 	}
 }
