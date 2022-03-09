c90b4f02af622ff2e3786210df0f7741b39ad92d
Doeke Norg <doekenorg@gmail.com>
2022-03-09 16:56:49 +0100

    First setup for retrieve correct values.

--- src/Field/NestedFormField.php
+++ src/Field/NestedFormField.php
@@ -18,10 +18,25 @@ class NestedFormField extends SeparableField implements RowsInterface {
 			$value = $entry[ $this->field->id ] ?? null;
 			$keys  = array_flip( $this->field->gpnfFields ?? [] );
 
-			// Map all entries to filter out the unwanted values.
-			yield from array_map( function ( array $entry ) use ( $keys ): array {
-				// Retrieve only the fields that are set on the form field.
-				return $this->wrap( array_values( array_intersect_key( $entry, $keys ) ) );
+            $nested_form = \GFAPI::get_form( $this->field->gpnfForm );
+
+            /** @var array<\GF_Field> $fields */
+            $fields = array_reduce( $nested_form['fields'], function (array $fields, \GF_Field $field ) {
+                if (in_array( $field->id, $this->field->gpnfFields ?? [], false )) {
+                    $fields[$field->id] = $field;
+
+                    return $fields;
+                }
+            }, []);
+
+			// Map all   entries to filter out the unwanted values.
+			yield from array_map( function ( array $entry ) use ( $keys, $fields ): array {
+               $values = array_intersect_key( $entry, $keys );
+
+                return $this->wrap( array_values( array_map(function($value, $field_id)  use ($entry, $fields) {
+                   return $fields[$field_id]->get_value_export( $entry, '', true, true );
+               }, $values, array_keys($values) ) ) );
+
 			}, \GP_Nested_Forms::get_instance()->get_entries( $value ) );
 		}
 	}
