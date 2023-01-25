884ec985ac655cccb97b5760e213891f1f936222
Zack Katz <zack@katz.co>
2023-01-25 15:19:32 -0500

    Only have one upgrade link in the plugin row

--- src/Component/Plugin.php
+++ src/Component/Plugin.php
@@ -15,38 +15,6 @@ final class Plugin {
 	 */
 	public function __construct() {
 		add_filter( 'plugin_row_meta', \Closure::fromCallable( [ $this, 'plugin_row_meta' ] ), 10, 2 );
-		add_filter( 'plugin_action_links', \Closure::fromCallable( [ $this, 'plugin_action_links' ] ), 10, 2 );
-	}
-
-	/**
-	 * Adds the settings link to the plugin row.
-	 * @since 1.8.0
-	 *
-	 * @param string[] $actions The action links.
-	 * @param string $plugin_file The name of the plugin file.
-	 *
-	 * @return string[] The new action links.
-	 */
-	private function plugin_action_links( array $actions, string $plugin_file ): array {
-		if ( plugin_basename( GFEXCEL_PLUGIN_FILE ) !== $plugin_file ) {
-			return $actions;
-		}
-
-		// Already has GravityExport
-		if ( class_exists( 'GravityKit\GravityExport\GravityExport' ) ) {
-			return $actions;
-		}
-
-		// Lite is active
-		if ( array_key_exists( 'deactivate', $actions ) ) {
-			$actions[] = implode( '', [
-				'<a target="_blank" rel="nofollow noopener" href="https://gravityview.co/extensions/gravityexport/"><b>⚡️ ',
-				esc_html__( 'Gain Access to More Features', 'gk-gravityexport-lite' ),
-				'</b></a>',
-			] );
-		}
-
-		return $actions;
 	}
 
 	/**
