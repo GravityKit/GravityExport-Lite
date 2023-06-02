b3e1632f17de0aa263b579f96671b27008759e5d
Doeke Norg <doekenorg@gmail.com>
2023-06-02 15:19:48 +0200

    up version no

--- src/Migration/Exception/NonBreakingMigrationException.php
+++ src/Migration/Exception/NonBreakingMigrationException.php
@@ -4,7 +4,7 @@ namespace GFExcel\Migration\Exception;
 
 /**
  * Exception that represents that something went wrong, but it should not break the migration (or process).
- * @since $ver$
+ * @since 2.0.1
  */
 final class NonBreakingMigrationException extends MigrationException {
 }
